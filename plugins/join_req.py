acdc1b2cc5b62af222d848d06abb3ab897453dbb
Pokkirispark1 <125215338+Pokkirispark1@users.noreply.github.com>
2025-07-08 19:05:08 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -19,14 +19,14 @@ async def join_reqs(client, join_req):
     if await fsub_db.check_rfsub_limit():
         join_count = await fsub_db.get_join_count()
         limit = await fsub_db.get_rfsub_limit()
-        limit_text = f"{join_count} of members" if limit else ""
-        await client.send_message(RFSUB_NOTIFICATION, f"<b>Fsᴜʙ Wᴏʀᴋ Cᴏᴍᴘʟᴇᴛᴇᴅ ✅</b>\n<b>Rᴇǫᴜᴇsᴛs ᴀᴅᴅᴇᴅ ➡️</b>{limit_text}\n<b>Cʜᴀɴɴᴇʟ ➡️</b> : <code>{channel_id}</code>\n\n<b>Fsᴜʙ sʜɪғᴛᴇᴅ ᴛᴏ ᴅᴇғᴀᴜʟᴛ ᴄʜᴀɴɴᴇʟ</b>") 
+        limit_text = f"{join_count}/{limit} members" if limit else f"{join_count} members"
+        await client.send_message(RFSUB_NOTIFICATION, f"<b>Fsᴜʙ Wᴏʀᴋ Cᴏᴍᴘʟᴇᴛᴇᴅ ✅</b>\n<b>Rᴇǫᴜᴇsᴛs ᴀᴅᴅᴇᴅ ➡️</b> {limit_text}\n<b>Cʜᴀɴɴᴇʟ ➡️</b> : <code>{channel_id}</code>\n\n<b>Fsᴜʙ sʜɪғᴛᴇᴅ ᴛᴏ ᴅᴇғᴀᴜʟᴛ ᴄʜᴀɴɴᴇʟ</b>")
         await client.send_sticker(RFSUB_NOTIFICATION, "CAACAgUAAxkBAAEO3WdoagEzQBPp9KB5TiIV9-rt-7qF_QACcQoAAmBffQVBn0D_9WK7hTYE")
         try:
-            await client.send_message(channel_id, f"Fsᴜʙ Wᴏʀᴋ Cᴏᴍᴘʟᴇᴛᴇᴅ ✅</b>\n<b>Rᴇǫᴜᴇsᴛs ᴀᴅᴅᴇᴅ ➡️</b>{limit_text}\n<b>Cʜᴀɴɴᴇʟ ➡️</b> : <code>{channel_id}</code>\n\n<b>Fsᴜʙ sʜɪғᴛᴇᴅ ᴛᴏ ᴅᴇғᴀᴜʟᴛ ᴄʜᴀɴɴᴇʟ</b>")
+            await client.send_message(channel_id, f"<b>Fsᴜʙ Wᴏʀᴋ Cᴏᴍᴘʟᴇᴛᴇᴅ ✅</b>\n<b>Rᴇǫᴜᴇsᴛs ᴀᴅᴅᴇᴅ ➡️</b> {limit_text}\n<b>Cʜᴀɴɴᴇʟ ➡️</b> : <code>{channel_id}</code>\n\n<b>Fsᴜʙ sʜɪғ�.tᴇᴅ ᴛᴏ ᴅᴇғᴀᴜʟ�.t ᴄʜᴀɴɴᴇʟ</b>")
         except Exception as e:
             await client.send_message(LOG_CHANNEL, f"Failed to notify fsub channel {channel_id}: {e}")
-    
+
     if str(user_id) in temp.AUTO_ACCEPT:
         file_id = temp.AUTO_ACCEPT[str(user_id)]['file_id']
         grp_id = temp.AUTO_ACCEPT[str(user_id)]['grp_id']
